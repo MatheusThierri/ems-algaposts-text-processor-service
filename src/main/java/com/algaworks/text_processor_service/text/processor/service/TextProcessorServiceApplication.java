e5dcf619530d91c50d90b8b8824961126d1dffe7
MatheusThierri <matheusthierri7@gmail.com>
2026-04-30 23:44:37 -0300

    Create projects.

---
+++ src/main/java/com/algaworks/text_processor_service/text/processor/service/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.text_processor_service.text.processor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/text_processor_service/text/processor/service/TextProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.text_processor_service.text.processor.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
